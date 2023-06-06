db5b73fb9b32d9005156670858039e7c714d7da7
Dr. Abiira Nathan <nabiira2by2@gmail.com>
2023-06-06 15:14:25 +0300

    Integrate clang-tidy

--- observable.h
+++ observable.h
@@ -1,7 +1,8 @@
 #ifndef OBSERVABLE_H
 #define OBSERVABLE_H
 
-#include "observable_global.h"
+#include <QCoreApplication>
+#include <QDir>
 #include <QObject>
 #include <QSettings>
 #include <QString>
@@ -10,74 +11,68 @@
 #include <iostream>
 #include <memory>
 #include <vector>
-#include <QCoreApplication>
 
-template <typename T> class OBSERVABLE_EXPORT Observable
-{
-public:
-    using Observer = std::function<void(const T &)>;
-    Observable<T>(){}
-    void subscribe(const Observer &observer){
-        observers.push_back(observer);
-    }
+#include "observable_global.h"
 
-    void next(const T &value){
-        for (const auto &observer : observers){
+template <typename T>
+class OBSERVABLE_EXPORT Observable {
+   public:
+    using Observer = std::function<void(const T&)>;
+    Observable<T>() {}
+    void subscribe(const Observer& observer) { observers.push_back(observer); }
+
+    void next(const T& value) {
+        for (const auto& observer : observers) {
             observer(value);
         }
     }
 
-private:
+   private:
     std::vector<Observer> observers;
 };
 
+template <typename T>
+class OBSERVABLE_EXPORT DataStore {
+   public:
+    DataStore<T>() {}
 
-template <typename T> class OBSERVABLE_EXPORT DataStore{
-public:
-    DataStore<T>(){}
-
-    void setValue(const T &value){
+    void setValue(const T& value) {
         data = value;
         observable.next(value);
     }
 
-    const T &getValue() const{
-        return data;
-    }
+    const T& getValue() const { return data; }
 
-    Observable<T> &getObservable(){
-        return observable;
-    }
+    Observable<T>& getObservable() { return observable; }
 
-private:
+   private:
     T data;
     Observable<T> observable;
 };
 
-
 // QSettingsWrapper
-class OBSERVABLE_EXPORT QSettingsWrapper : public QObject
-{
+class OBSERVABLE_EXPORT QSettingsWrapper : public QObject {
     Q_OBJECT
-public:
-    QSettingsWrapper(QObject *parent = nullptr) : QObject(parent){}
-
-    void setValue(const QString &key, const QVariant &value)
-    {
+   public:
+    QSettingsWrapper(QObject* parent = nullptr) : QObject(parent) {}
+    void setValue(const QString& key, const QVariant& value) {
         settings.setValue(key, value);
         emit preferenceChanged(key, value);
     }
 
-    QVariant getValue(const QString &key, const QVariant &defaultValue = QVariant()) const
-    {
+    QVariant getValue(const QString& key, const QVariant& defaultValue = QVariant()) const {
         return settings.value(key, defaultValue);
     }
 
-signals:
-    void preferenceChanged(const QString &key, const QVariant &value);
+    // Returns a reference to the underlying settings object
+    QSettings& getSettings() { return settings; }
 
-private:
+   signals:
+    void preferenceChanged(const QString& key, const QVariant& value);
+
+   private:
+    QString settingsFile;
     QSettings settings;
 };
 
-#endif // OBSERVABLE_H
+#endif  // OBSERVABLE_H
